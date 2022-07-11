2ace5c5a79586eb88ff13cadf965603b2ae88ce6
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2022-07-11 22:44:39 +0900

    add app

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [playState, setPlayState] = useState(false);
+  const [timerValue, setTimerValue] = useState("00:00:00");
+  const ourInterval = useRef(undefined);
+
+  const toggleState = () => {
+    if (playState) {
+      setPlayState(false);
+      clearInterval(ourInterval.current);
+      ourInterval.current = undefined;
+      setTimerValue("00:00:00");
+    } else {
+      setPlayState(true);
+      let numSeconds = 0;
+      ourInterval.current = setInterval(() => {
+        numSeconds += 1;
+        let date_str = new Date(numSeconds * 1000).toISOString();
+        setTimerValue(date_str.substring(11, 19));
+      }, 1000);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{timerValue}</Text>
+      <TouchableOpacity style={[styles.touchableOpacity_start, playState == false ? styles.touchableOpacity_start : styles.touchableOpacity_stop]} onPress={() => toggleState()}>
+        <Text style={[styles.touchableOpacityText_start, playState == false ? styles.touchableOpacityText_start : styles.touchableOpacityText_stop]}>{playState == false ? "START" : "STOP"}</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 80,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 50
+  },
+  touchableOpacity_stop: {
+    backgroundColor: "black",
+    borderWidth: 7,
+    borderColor: "red"
+  },
+  touchableOpacity_start: {
+    width: "80%",
+    height: 80,
+    borderRadius: 50,
+    margin: 10,
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  touchableOpacityText_start: {
+    color: "black",
+    fontSize: 30,
+    fontWeight: "bold",
+    width: "100%",
+    textAlign: "center"
+  },
+  touchableOpacityText_stop: {
+    color: "red",
   },
 });
